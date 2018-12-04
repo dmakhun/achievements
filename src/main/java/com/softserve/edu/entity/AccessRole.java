006a7fdb401db508dc8f74596555b88a08848c8e
dmakhun <dmytro.makhun@gmail.com>
2018-12-04 19:37:21 +0200

    upgraded libs to newest: Spring Security, log4j

--- src/main/java/com/softserve/edu/entity/AccessRole.java
+++ src/main/java/com/softserve/edu/entity/AccessRole.java
@@ -16,8 +16,7 @@ import javax.persistence.Table;
 @Table(name = "AccessRole")
 @NamedQueries({
         @NamedQuery(name = AccessRole.FIND_USERS_BY_ROLE_ID, query = AccessRole.FIND_USERS_BY_ROLE_ID_QUERY),
-        @NamedQuery(name = AccessRole.FIND_ROLE_BY_NAME, query = AccessRole.FIND_ROLE_BY_NAME_QUERY),
-        @NamedQuery(name = AccessRole.FIND_USERS_BY_ROLE_UUID, query = AccessRole.FIND_USERS_BY_ROLE_UUID_QUERY)
+        @NamedQuery(name = AccessRole.FIND_ROLE_BY_NAME, query = AccessRole.FIND_ROLE_BY_NAME_QUERY)
 })
 public class AccessRole extends AbstractEntity {
 
@@ -27,77 +26,36 @@ public class AccessRole extends AbstractEntity {
     public static final String FIND_ROLE_BY_NAME = "AccessRole.findRoleId";
     static final String FIND_ROLE_BY_NAME_QUERY = "from AccessRole where name like ?1";
 
-    public static final String FIND_USERS_BY_ROLE_UUID = "ROLE";
-    public static final String FIND_USERS_BY_ROLE_UUID_QUERY = "from User u INNER JOIN fetch u.role r WHERE r.uuid	like ?1";
-
-
-    /**
-     * Id field. It's PK and must be generated value. Mapped on column id.
-     */
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    /**
-     * name field. Mapped on column name.
-     */
     @Column(name = "name", length = 50)
     private String name;
 
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "accessRole")
     private Set<User> user;
 
-    /**
-     * Default constructor.
-     */
     public AccessRole() {
-
     }
-
-    /**
-     * Parameterized constructor.
-     *
-     * @param name value for name field
-     */
 
     public AccessRole(String name) {
         this.name = name;
     }
 
-    /**
-     * Gets value of id field.
-     *
-     * @return id
-     */
-
     public Long getId() {
         return id;
     }
 
-    /**
-     * Sets setId field.
-     *
-     * @param id value for id field
-     */
     public void setId(Long id) {
         this.id = id;
     }
 
-    /**
-     * Gets value of id name.
-     *
-     * @return name
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     * Sets setName field.
-     *
-     * @param name value for name field
-     */
     public void setName(String name) {
         this.name = name;
     }
