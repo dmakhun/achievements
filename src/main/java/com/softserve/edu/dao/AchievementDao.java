2be0dbe1574464c7d94d3315e254412d5a3228fe
dmakhun <dmytro.makhun@gmail.com>
2018-12-04 16:56:03 +0200

    - deleted uuid from abstract entity - deleted @XmlTransient annotation

--- src/main/java/com/softserve/edu/dao/AchievementDao.java
+++ src/main/java/com/softserve/edu/dao/AchievementDao.java
@@ -4,9 +4,10 @@ import com.softserve.edu.entity.Achievement;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface AchievementDao extends CrudRepository<Achievement, Long> {
 
     @Query("select a from Achievement a where a.user.id = :userId")
-    List<Achievement> findAchievementsByUserId(long userId);
+    List<Achievement> findAchievementsByUserId(@Param("userId") long userId);
 }
