812e49b8ab7e80daf5b803647922eb886f7f10be
dmakhun <dmytro.makhun@gmail.com>
2018-12-09 02:15:50 +0200

    switched to spring data method

--- src/main/java/com/softserve/edu/dao/AchievementDao.java
+++ src/main/java/com/softserve/edu/dao/AchievementDao.java
@@ -2,12 +2,10 @@ package com.softserve.edu.dao;
 
 import com.softserve.edu.entity.Achievement;
 import java.util.List;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface AchievementDao extends CrudRepository<Achievement, Long> {
 
-    @Query("select a from Achievement a where a.user.id = :userId")
-    List<Achievement> findAchievementsByUserId(@Param("userId") long userId);
+    List<Achievement> findAchievementsByUserId(long userId);
+
 }
