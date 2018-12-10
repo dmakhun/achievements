41761d7ae73f51f82b3b74b85f471a2b80a34d52
dmakhun <dmytro.makhun@gmail.com>
2018-12-10 18:57:44 +0200

    CompetenceDao refactorings

--- src/main/java/com/softserve/edu/dao/CompetenceDao.java
+++ src/main/java/com/softserve/edu/dao/CompetenceDao.java
@@ -1,25 +1,14 @@
 package com.softserve.edu.dao;
 
-import com.softserve.edu.entity.Class;
 import com.softserve.edu.entity.Competence;
 import java.util.List;
 
 public interface CompetenceDao extends GenericDao<Competence> {
 
-    List<Class> findGroupsByCompetenceId(int competenceId);
-
     Competence findByName(String name);
 
-    /**
-     * Get list of competences, that user wants to attend to.
-     */
     List<Competence> findCompetencesByUserId(Long userId);
 
-    /**
-     * Get competences with loaded users to them.
-     *
-     * @return list of competences
-     */
     List<Competence> findAllCompetences();
 
 }
