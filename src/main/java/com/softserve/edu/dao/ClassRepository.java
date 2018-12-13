6c1088d9b576ab8b36c0a09e138f12447a75777f
dmakhun <dmytro.makhun@gmail.com>
2018-12-13 19:03:06 +0200

    created group repository added draft method

--- src/main/java/com/softserve/edu/dao/ClassRepository.java
+++
@@ -1,8 +0,0 @@
-package com.softserve.edu.dao;
-
-import com.softserve.edu.entity.Group;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClassRepository extends CrudRepository<Group, Long> {
-
-}
