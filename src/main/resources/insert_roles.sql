d349d0124bc22e5c4c1e9a3cc24e1b13dcdb461e
dmakhun <dmytro.makhun@gmail.com>
2018-12-13 16:38:30 +0200

    added default users to fill-in DB script

--- src/main/resources/insert_roles.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO accessrole (id, LastModifiedDate, name) VALUES (1, NOW(), 'ROLE_ADMIN');
-INSERT INTO accessrole (id, LastModifiedDate, name) VALUES (2, NOW(), 'ROLE_USER');
-INSERT INTO accessrole (id, LastModifiedDate, name) VALUES (3, NOW(), 'ROLE_MANAGER');
-INSERT INTO accessrole (id, LastModifiedDate, name) VALUES (4, NOW(), 'ROLE_ANONYMOUS');
-
