dc5a57317b570e604c15c80e68eea2ae0f2ee046
dmakhun <dmytro.makhun@gmail.com>
2018-12-13 15:33:37 +0200

    modified roles scripts id

--- src/main/resources/insert_roles.sql
+++ src/main/resources/insert_roles.sql
@@ -1,4 +1,5 @@
-INSERT INTO accessrole (id, LastModifiedDate, name) VALUES (1, NOW(), 'ROLE_USER');
-INSERT INTO accessrole (id, LastModifiedDate, name) VALUES (2, NOW(), 'ROLE_MANAGER');
-INSERT INTO accessrole (id, LastModifiedDate, name) VALUES (3, NOW(), 'ROLE_ADMIN');
+INSERT INTO accessrole (id, LastModifiedDate, name) VALUES (1, NOW(), 'ROLE_ADMIN');
+INSERT INTO accessrole (id, LastModifiedDate, name) VALUES (2, NOW(), 'ROLE_USER');
+INSERT INTO accessrole (id, LastModifiedDate, name) VALUES (3, NOW(), 'ROLE_MANAGER');
 INSERT INTO accessrole (id, LastModifiedDate, name) VALUES (4, NOW(), 'ROLE_ANONYMOUS');
+
