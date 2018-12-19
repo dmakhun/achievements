22058ecf07b0b847d49076dbdf0e4d98757429fe
dmakhun <dmytro.makhun@gmail.com>
2018-12-19 21:01:39 +0200

    removed UserDao

--- src/main/java/com/softserve/edu/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.edu.dao;
-
-import com.softserve.edu.entity.User;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface UserDao extends GenericDao<User> {
-
-}
