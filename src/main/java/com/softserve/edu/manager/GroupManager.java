75f702cc0b21fc3af7e410b81dcee7a2c5b37194
dmakhun <dmytro.makhun@gmail.com>
2018-12-21 23:33:35 +0200

    refactorings

--- src/main/java/com/softserve/edu/manager/GroupManager.java
+++ src/main/java/com/softserve/edu/manager/GroupManager.java
@@ -6,27 +6,12 @@ import java.util.Date;
 
 public interface GroupManager {
 
-    /**
-     * Create new group.
-     */
     Long create(String name, Date startDate,
             Date endDate, Long competenceId) throws GroupManagerException;
 
-    /**
-     * Modify existing group.
-     *
-     * @param groupId ID of existing group.
-     * @param name Group name.
-     * @param start Opening date.
-     * @param end Ending date.
-     * @param competenceId Competence id.
-     */
     void modify(Long groupId, String name, Date start,
             Date end, Long competenceId) throws GroupManagerException;
 
-    /**
-     * Add existing user to existing group.
-     */
     void addUser(Long userId, Long groupId) throws GroupManagerException;
 
     void create(Group aGroup) throws GroupManagerException;
