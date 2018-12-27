d846ff91baed56befbb0bf9df83fd743039d56f6
dmakhun <dmytro.makhun@gmail.com>
2018-12-27 19:01:50 +0200

    started to implement dynamic search

--- src/main/java/com/softserve/edu/dao/UserRepository.java
+++ src/main/java/com/softserve/edu/dao/UserRepository.java
@@ -1,10 +1,18 @@
 package com.softserve.edu.dao;
 
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
+import com.softserve.edu.entity.QUser;
 import com.softserve.edu.entity.User;
 import java.util.List;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.SingleValueBinding;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long>,
+        QuerydslPredicateExecutor<User>, QuerydslBinderCustomizer<QUser> {
 
     List<User> findByGroups_Id(Long groupId);
 
@@ -13,5 +21,18 @@ public interface UserRepository extends CrudRepository<User, Long> {
     User findByEmail(String email);
 
     List<User> findByRoleName(String roleName);
+
+/*    @Query("SELECT u from User u where ")
+    List<User> findAllManagers();*/
+
+    @Override
+    default void customize(QuerydslBindings bindings, QUser user) {
+
+        bindings.bind(user.name).first(StringExpression::containsIgnoreCase);
+        bindings.bind(user.surname).first(StringExpression::containsIgnoreCase);
+        bindings.bind(user.username).first(StringExpression::containsIgnoreCase);
+        bindings.bind(String.class)
+                .first((SingleValueBinding<StringPath, String>) StringExpression::containsIgnoreCase);
+    }
 
 }
