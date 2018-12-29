646718cea52e4347579fabdd3bc839496c1d66c7
dmakhun <dmytro.makhun@gmail.com>
2018-12-30 00:18:06 +0200

    added dynamic search using Querydsl and Pageable

--- src/main/java/com/softserve/edu/dao/GroupRepository.java
+++ src/main/java/com/softserve/edu/dao/GroupRepository.java
@@ -8,13 +8,13 @@ import org.springframework.data.repository.query.Param;
 
 public interface GroupRepository extends CrudRepository<Group, Long> {
 
-    @Query("SELECT g from Group g where g.dateOpened > current_date and g.competence.id = :competenceId")
+    @Query("SELECT g from Group g where g.opened > current_date and g.competence.id = :competenceId")
     List<Group> findPendingByCompetenceId(@Param("competenceId") Long competenceId);
 
-    @Query("SELECT g from Group g where g.dateClosed > current_date and g.competence.id = :competenceId")
+    @Query("SELECT g from Group g where g.closed > current_date and g.competence.id = :competenceId")
     List<Group> findOpenedByCompetenceId(@Param("competenceId") Long competenceId);
 
-    @Query("SELECT g from Group g join g.users u where g.dateClosed > current_date and u.id = :userId")
+    @Query("SELECT g from Group g join g.users u where g.closed > current_date and u.id = :userId")
     List<Group> findOpenedByUserId(@Param("userId") Long userId);
 
     Group findByName(String name);
