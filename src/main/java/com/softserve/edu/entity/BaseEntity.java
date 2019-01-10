06d96dd10cec880143bad16375cfae9dc1828765
dmakhun <dmytro.makhun@gmail.com>
2019-01-10 13:21:54 +0200

    rename last_modified_date column

--- src/main/java/com/softserve/edu/entity/BaseEntity.java
+++ src/main/java/com/softserve/edu/entity/BaseEntity.java
@@ -1,17 +1,22 @@
 package com.softserve.edu.entity;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
-@EntityListeners(Listener.class)
+@EntityListeners(EntityListener.class)
 public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "LastModifiedDate")
+    @Column(name = "last_modified_date")
     private LocalDateTime lastModifiedDate;
 
     public LocalDateTime getLastModifiedDate() {
