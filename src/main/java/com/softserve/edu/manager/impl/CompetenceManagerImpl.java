295fdabb4f0b03e44080aa922a918eb6a6d8b1ae
dmakhun <dmytro.makhun@gmail.com>
2019-03-03 00:21:15 +0200

    minor refactorings

--- src/main/java/com/softserve/edu/manager/impl/CompetenceManagerImpl.java
+++ src/main/java/com/softserve/edu/manager/impl/CompetenceManagerImpl.java
@@ -3,7 +3,6 @@ package com.softserve.edu.manager.impl;
 import com.softserve.edu.dao.CompetenceRepository;
 import com.softserve.edu.entity.Competence;
 import com.softserve.edu.manager.CompetenceManager;
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,8 +24,7 @@ public class CompetenceManagerImpl implements CompetenceManager {
     @Override
     @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public List<Competence> findExcept(List<Competence> competencesToExclude) {
-        List<Competence> competences = new ArrayList<>();
-        competenceRepository.findAll().forEach(competences::add);
+        List<Competence> competences = competenceRepository.findAll();
         competences.removeAll(competencesToExclude);
         return competences;
     }
