885be916120fc609ced207210e976bee06428209
dmakhun <dmytro.makhun@gmail.com>
2019-03-16 19:34:54 +0200

    refactorings

--- src/main/java/com/softserve/edu/manager/CompetenceManager.java
+++ src/main/java/com/softserve/edu/manager/CompetenceManager.java
@@ -1,18 +1,17 @@
 package com.softserve.edu.manager;
 
 import com.softserve.edu.entity.Competence;
+import com.softserve.edu.entity.User;
 import java.util.List;
 
 public interface CompetenceManager {
 
-    /**
-     * @return all list of competenceies.
-     */
     List<Competence> findAllCompetences();
 
     /**
-     * Get list of competences but those, that are given.
+     * Find competences that given user can take.
+     * @param user
      */
-    List<Competence> findExcept(List<Competence> buts);
+    List<Competence> findAvailable(User user);
 
 }
