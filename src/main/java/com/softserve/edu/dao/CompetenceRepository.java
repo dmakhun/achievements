2ad60d8d91cb9bdd29e597653df0b721feca1104
dmakhun <dmytro.makhun@gmail.com>
2019-03-17 01:48:07 +0200

    optimized querying of opened groups

--- src/main/java/com/softserve/edu/dao/CompetenceRepository.java
+++ src/main/java/com/softserve/edu/dao/CompetenceRepository.java
@@ -3,10 +3,14 @@ package com.softserve.edu.dao;
 import com.softserve.edu.entity.Competence;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CompetenceRepository extends JpaRepository<Competence, Long> {
 
     Competence findByName(String name);
 
     List<Competence> findByUsers_Id(Long userId);
+
+    @Query("SELECT c from Competence c join c.groups g where g.dateClosed > current_date")
+    List<Competence> findWithOpenedGroups();
 }
