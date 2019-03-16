d3bad996c2a62ec789ffcc58ecadf2617019ea42
dmakhun <dmytro.makhun@gmail.com>
2019-03-16 20:58:04 +0200

    Competence Controller/View minor refactorings

--- src/main/java/com/softserve/edu/controller/CompetenceController.java
+++ src/main/java/com/softserve/edu/controller/CompetenceController.java
@@ -1,14 +1,14 @@
 package com.softserve.edu.controller;
 
+import static com.softserve.edu.util.Constants.GENERAL_ERROR;
+
 import com.softserve.edu.dao.CompetenceRepository;
 import com.softserve.edu.dao.GroupRepository;
 import com.softserve.edu.entity.Competence;
-import com.softserve.edu.entity.Group;
 import com.softserve.edu.exception.UserManagerException;
 import com.softserve.edu.manager.CompetenceManager;
 import com.softserve.edu.manager.UserManager;
 import java.time.LocalDate;
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,9 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class CompetenceController {
 
-    private static final String GENERALERROR = "redirect:/myerror/10";
-    private static final Logger logger = LoggerFactory
-            .getLogger(CompetenceController.class);
+    private static final Logger logger = LoggerFactory.getLogger(CompetenceController.class);
 
     @Autowired
     private GroupRepository groupRepository;
@@ -38,14 +35,13 @@ public class CompetenceController {
     private UserManager userManager;
 
     @RequestMapping(value = "/competence", method = RequestMethod.GET)
-    public String groups(Model model) {
+    public String allCompetences(Model model) {
         try {
-            List<Competence> competences = competenceManager.findAllCompetences();
-            model.addAttribute("competences", competences);
+            model.addAttribute("competences", competenceManager.findAllCompetences());
             return "showCompetence";
         } catch (Exception e) {
             logger.error(e.getMessage());
-            return GENERALERROR;
+            return GENERAL_ERROR;
         }
     }
 
@@ -59,11 +55,12 @@ public class CompetenceController {
             return "addCompetence";
         } catch (Exception e) {
             logger.error(e.getMessage());
-            return GENERALERROR;
+            return GENERAL_ERROR;
         }
     }
 
-    @PostMapping(value = "/addCompetence", params = {"addCompetence"})
+    @RequestMapping(value = "/addCompetence", params = {
+            "addCompetence"}, method = RequestMethod.POST)
     public String addCompetence(
             @RequestParam(value = "competenceName", required = false, defaultValue = "") String name) {
         competenceRepository.save(new Competence(name, LocalDate.now()));
@@ -72,41 +69,39 @@ public class CompetenceController {
 
     @RequestMapping(value = "/addManagerList", method = RequestMethod.POST)
     public String addManager(
-            @RequestParam(value = "userlist", required = false, defaultValue = "") Long userId,
-            @RequestParam(value = "rolelist", required = false, defaultValue = "") Long roleId) {
+            @RequestParam(value = "userId", required = false, defaultValue = "") Long userId,
+            @RequestParam(value = "roleId", required = false, defaultValue = "") Long roleId) {
         try {
             userManager.updateUser(userId, null, null, null, null, null, roleId);
             return "redirect:/competencies/addManager?status=success";
         } catch (UserManagerException e) {
             logger.error(e.getMessage());
-            return GENERALERROR;
+            return GENERAL_ERROR;
         }
     }
 
     @RequestMapping(value = "/manager/competence", method = RequestMethod.GET)
-    public String getGroups(Model model) {
+    public String allCompetencesGroups(Model model) {
         try {
-            List<Competence> allCompetences = competenceManager.findAllCompetences();
-            model.addAttribute("allCompetences", allCompetences);
+            model.addAttribute("allCompetences", competenceManager.findAllCompetences());
             return "groupsAndCompetence";
         } catch (Exception e) {
             logger.error(e.getMessage());
-            return GENERALERROR;
+            return GENERAL_ERROR;
         }
     }
 
     @RequestMapping(value = "/manager/competence", method = RequestMethod.POST)
-    public String getGroupsPost(
+    public String allCompetencesGroups(
             @RequestParam(value = "competence") Long competenceId, Model model) {
         try {
-            List<Competence> allCompetences = competenceManager.findAllCompetences();
-            List<Group> openedGroups = groupRepository.findOpenedByCompetenceId(competenceId);
-            model.addAttribute("allCompetences", allCompetences);
-            model.addAttribute("openedGroups", openedGroups);
+            model.addAttribute("allCompetences", competenceManager.findAllCompetences());
+            model.addAttribute("openedGroups",
+                    groupRepository.findOpenedByCompetenceId(competenceId));
             return "groupsAndCompetence";
         } catch (Exception e) {
             logger.error(e.getMessage());
-            return GENERALERROR;
+            return GENERAL_ERROR;
         }
     }
 
@@ -116,14 +111,13 @@ public class CompetenceController {
             @RequestParam(value = "statusRemove", required = false, defaultValue = "") String statusRemove,
             @RequestParam(value = "statusAdd", required = false, defaultValue = "") String statusAdd) {
         try {
-            List<Competence> competences = competenceManager.findAllCompetences();
-            model.addAttribute("statusRemove", statusRemove);
+            model.addAttribute("allCompetences", competenceManager.findAllCompetences());
             model.addAttribute("statusAdd", statusAdd);
-            model.addAttribute("competencelist", competences);
+            model.addAttribute("statusRemove", statusRemove);
             return "forDeleteOrAddCompetence";
         } catch (Exception e) {
             logger.error(e.getMessage());
-            return GENERALERROR;
+            return GENERAL_ERROR;
         }
 
     }
