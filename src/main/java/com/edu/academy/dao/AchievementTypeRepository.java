2de4a5ba4b209b8653adc0eb0dcef17186b634b1
Dmytro Makhun <dmytro.makhun@gmail.com>
2021-04-14 01:24:01 +0300

    - added annotation to repositories - changed all to CrudRepository

--- src/main/java/com/edu/academy/dao/AchievementTypeRepository.java
+++ src/main/java/com/edu/academy/dao/AchievementTypeRepository.java
@@ -1,11 +1,13 @@
 package com.edu.academy.dao;
 
 import com.edu.academy.entity.AchievementType;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface AchievementTypeRepository extends JpaRepository<AchievementType, Long> {
+@Repository
+public interface AchievementTypeRepository extends CrudRepository<AchievementType, Long> {
 
     List<AchievementType> findByCompetenceId(Long competenceId);
 
--- src/main/java/com/edu/academy/dao/GroupRepository.java
+++ src/main/java/com/edu/academy/dao/GroupRepository.java
@@ -1,13 +1,15 @@
 package com.edu.academy.dao;
 
 import com.edu.academy.entity.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface GroupRepository extends JpaRepository<Group, Long> {
+@Repository
+public interface GroupRepository extends CrudRepository<Group, Long> {
 
     @Query("SELECT g from Group g where g.dateOpened > current_date and g.competence.id = :competenceId")
     List<Group> findPendingByCompetenceId(@Param("competenceId") Long competenceId);
