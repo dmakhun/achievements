3feaafa0e9b3087d7e66c8a157b06e4051653d06
Dmytro Makhun <dmytro.makhun@gmail.com>
2021-04-17 20:21:45 +0300

    Reverted JpaRepositories - Crud returns Iterable and shpuld be adjusted separately

--- src/main/java/com/edu/academy/dao/AchievementTypeRepository.java
+++ src/main/java/com/edu/academy/dao/AchievementTypeRepository.java
@@ -1,13 +1,13 @@
 package com.edu.academy.dao;
 
 import com.edu.academy.entity.AchievementType;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface AchievementTypeRepository extends CrudRepository<AchievementType, Long> {
+public interface AchievementTypeRepository extends JpaRepository<AchievementType, Long> {
 
     List<AchievementType> findByCompetenceId(Long competenceId);
 
